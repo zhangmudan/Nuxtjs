557acf2f069b062af917b04cf93f5c7e814e2ad8
张牡丹 <1050339760@qq.com>
2020-04-16 19:27:16 +0800

    初始化项目

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 类似于router-view 匹配到组件的坑位 -->
+    <nuxt />
+  </div>
+</template>
+
+<style lang='less'>
+//公共样式
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  //inherit 继承父元素的样式
+  color: inherit;
+  text-decoration: none;
+}
+a:hover {
+  color: inherit;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+i,
+em {
+  font-style: normal;
+}
+input,
+button {
+  outline: none;
+}
+</style>
---
+++ pages/user/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>用户登录注册页</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

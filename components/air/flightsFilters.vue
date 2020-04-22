7b750508b4522ac46b7cbcf475e6267fa0bf6b2d
张牡丹 <1050339760@qq.com>
2020-04-22 12:00:38 +0800

    机票单个条件过滤

---
+++ components/air/flightsFilters.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="filters">
+    <el-row
+      type="flex"
+      class="filters-top"
+      justify="space-between"
+      align="middle"
+    >
+      <el-col :span="8">
+        单程： {{ data.info.departCity }} - {{ data.info.destCity }} /
+        {{ data.info.departDate }}
+      </el-col>
+      <el-col :span="4">
+        <el-select
+          size="mini"
+          v-model="airport"
+          placeholder="起飞机场"
+          @change="handleAirport"
+        >
+          <el-option
+            v-for="(item, index) in data.options.airport"
+            :key="index"
+            :label="item"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="4">
+        <el-select
+          size="mini"
+          v-model="flightTimes"
+          placeholder="起飞时间"
+          @change="handleFlightTimes"
+        >
+          <el-option
+            v-for="(item, index) in data.options.flightTimes"
+            :key="index"
+            :label="`${item.from}:00 - ${item.to}:00`"
+            :value="`${item.from},${item.to}`"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="4">
+        <el-select
+          size="mini"
+          v-model="company"
+          placeholder="航空公司"
+          @change="handleCompany"
+        >
+          <el-option
+            v-for="(item, index) in data.options.company"
+            :key="index"
+            :label="item"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="4">
+        <el-select
+          size="mini"
+          v-model="airSize"
+          placeholder="机型"
+          @change="handleAirSize"
+        >
+          <el-option
+            v-for="(item, index) in airSizeList"
+            :key="index"
+            :label="item.labels"
+            :value="item.size"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <div class="filter-cancel">
+      筛选：
+      <el-button
+        type="primary"
+        round
+        plain
+        size="mini"
+        @click="handleFiltersCancel"
+      >
+        撤销
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      airport: "", // 机场
+      flightTimes: "", // 出发时间
+      company: "", // 航空公司
+      airSize: "", // 机型大小
+      airSizeList: [
+        { labels: "大", size: "L" },
+        { labels: "中", size: "M" },
+        { labels: "小", size: "S" }
+      ]
+    };
+  },
+  props: {
+    data: {
+      type: Object,
+      // 默认是空数组
+      default: {}
+    }
+  },
+  methods: {
+    // 选择机场时候触发
+    handleAirport(value) {
+      const arr = this.data.flights.filter(v => {
+        return v.org_airport_name === this.airport;
+      });
+      this.$emit("getFilter", arr);
+    },
+
+    // 选择出发时间时候触发
+    handleFlightTimes(value) {
+      console.log(value);
+
+      const arr = this.data.flights.filter(v => {
+        return v.dep_time === this.flightTimes;
+      });
+      this.$emit("getFilter", arr);
+    },
+
+    // 选择航空公司时候触发
+    handleCompany(value) {
+      const arr = this.data.flights.filter(v => {
+        return v.airline_name === this.company;
+      });
+      this.$emit("getFilter", arr);
+    },
+
+    // 选择机型时候触发
+    handleAirSize(value) {
+      const arr = this.data.flights.filter(v => {
+        return v.plane_size === this.airSize;
+      });
+      this.$emit("getFilter", arr);
+    },
+
+    // 撤销条件时候触发
+    handleFiltersCancel() {
+      (this.airport = ""), // 机场
+        (this.flightTimes = ""), // 出发时间
+        (this.company = ""), // 航空公司
+        (this.airSize = ""); // 机型大小
+      this.$emit("getFilter", this.data.flights);
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.filters {
+  margin-bottom: 20px;
+}
+
+.filters-top {
+  > div {
+    /deep/ .el-select {
+      margin-left: 10px;
+    }
+  }
+}
+
+.filter-cancel {
+  margin-top: 10px;
+}
+</style>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -3,15 +3,15 @@
     <el-row type="flex" justify="space-between">
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
-        <!-- 过滤条件 -->
-        <div></div>
+        <!-- 过滤条件  传备份数据 这样传的数据一直都是完整数据-->
+        <FlightsFilters :data="flightsLisCache" @getFilter="getFilter" />
 
         <!-- 航班头部布局 -->
         <FlightsListHead />
 
         <!-- 航班信息 -->
         <FlightsItem
-          v-for="(item, index) in flightsList"
+          v-for="(item, index) in flightsArr"
           :key="index"
           :data="item"
         />
@@ -38,21 +38,31 @@
 <script>
 import FlightsListHead from "@/components/air/flightsListHead";
 import FlightsItem from "@/components/air/flightsItem";
+import FlightsFilters from "@/components/air/flightsFilters";
 export default {
   data() {
     return {
       //航班总数据
-      flights: [],
+      flightsList: {
+        flights: [],
+        info: {},
+        options: {}
+      },
+      //备份数据
+      flightsLisCache: {
+        flights: [],
+        info: {},
+        options: {}
+      },
       pageSize: 5,
       pageIndex: 1,
-      total: 0,
-      // 切割后数据
-      flightsList: []
+      total: 0
     };
   },
   components: {
     FlightsItem,
-    FlightsListHead
+    FlightsListHead,
+    FlightsFilters
   },
   mounted() {
     this.$axios({
@@ -60,12 +70,21 @@ export default {
       params: this.$route.query
     }).then(res => {
       // console.log(res);
-      const { flights } = res.data;
-      this.flights = flights;
-      this.total = flights.length;
-      //[0,1,2,3,4,5,6,7,8].slice(0,5)=[0,1,2,3,4]
-      this.flightsList = flights.slice(0, this.pageSize);
+      this.flightsList = res.data;
+      //备份数据不能直接赋值,因为赋值的是同一个内存地址,当上一个更改时,备份数据也会被改
+      this.flightsLisCache = { ...res.data };
+      this.total = this.flightsList.total;
     });
+  },
+  computed: {
+    // 切割后数据
+    flightsArr() {
+      const arr = this.flightsList.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageSize * this.pageIndex
+      );
+      return arr;
+    }
   },
   methods: {
     //切换条数,页面数据
@@ -74,14 +93,13 @@ export default {
       this.pageSize = val;
       //切换条数时,当前页应该回到第一页
       this.pageIndex = 1;
-      this.flightsList = this.flights.slice((this.pageIndex - 1) * val, val);
     },
     //切换页数,页面数据
     handleCurrentChange(val) {
-      this.flightsList = this.flights.slice(
-        (val - 1) * this.pageSize,
-        val * this.pageSize
-      );
+      this.pageIndex = val;
+    },
+    getFilter(arr) {
+      this.flightsList.flights = arr;
     }
   }
 };
